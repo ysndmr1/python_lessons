fbb362ae4ce6175aca69200508c9bf34a6db3b61
ysndmr1 <ysndmr000@gmail.com>
2023-05-22 20:09:07 +0200

    flightApp

---
+++ 019_FlightAPI/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+urlpatterns = [
+     path('auth/', include('dj_rest_auth.urls'))
+]
+
+# ---------- Router ----------
+from rest_framework.routers import DefaultRouter
+from .views import UserView
+router = DefaultRouter()
+router.register('', UserView)
+urlpatterns += router.urls
