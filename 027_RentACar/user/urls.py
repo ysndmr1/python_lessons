947d608ad66446d71e667418b8688393e49e6df1
ysndmr1 <ysndmr000@gmail.com>
2023-06-12 20:01:15 +0200

    RentaCar

---
+++ 027_RentACar/user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+# afeter'/user/' isteklerinden sonra
+urlpatterns = [path("auth/", include("dj_rest_auth.urls"))]
+
+
+# ---------------------
+# Router
+# ---------------------
+from rest_framework.routers import DefaultRouter
+from .views import UserView
+
+router = DefaultRouter()
+router.register("", UserView)
+urlpatterns += router.urls
